518780c6696525c92d90af8043a70115baa180e4
LeThanhTung <thanhtung5598@gmail.com>
2020-02-07 18:49:08 +0700

    React CSS Transition Group

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+
+  state = {
+    items: ['Click', 'To', 'Remove', 'An', 'Item'],
+    inputText: ''
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      items: [...this.state.items, this.state.inputText],
+      inputText: '',
+    })
+  }
+
+  renderItems() {
+    return this.state.items.map((item, i) => {
+      return (
+        <div
+          className="item"
+          key={i}
+          onClick={() => this.removeItem(i)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {item}
+        </div>
+      );
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({ inputText: event.target.value })
+  }
+
+  removeItem(i) {
+    let newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({
+      items: newItems
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Animation-App">
+          <form onSubmit={this.handleSubmit}>
+            <input placeholder="Enter new Item" type="text" onChange={this.handleChange} value={this.state.inputText} />
+            <input type="submit" value="Add new Item"/>
+          </form>
+          <ReactCSSTransitionGroup transitionName="AppExp">
+            {this.renderItems()}
+          </ReactCSSTransitionGroup>
+        </div>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
