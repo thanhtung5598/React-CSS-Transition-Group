f70df5944349d2a1ed3b82b8dce03762bff2f0db
LeThanhTung <thanhtung5598@gmail.com>
2021-02-03 17:12:19 +0700

    Updated Transition

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
-import React, { Component } from 'react';
-import ReactCSSTransitionGroup from "react-addons-css-transition-group";
-import './App.css';
+import React, { Component } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import "./App.css";
 
 class App extends Component {
-
   state = {
-    items: ['Click', 'To', 'Remove', 'An', 'Item'],
-    inputText: ''
-  }
+    items: ["Click", "To", "Remove", "An", "Item"],
+    inputText: "",
+  };
 
   handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     this.setState({
       items: [...this.state.items, this.state.inputText],
-      inputText: '',
-    })
-  }
+      inputText: "",
+    });
+  };
 
   renderItems() {
     return this.state.items.map((item, i) => {
       return (
-        <div
-          className="item"
-          key={i}
-          onClick={() => this.removeItem(i)}
-        >
-          {item}
-        </div>
+        <CSSTransition key={i} appear={true} timeout={500} classNames="AppExp">
+          <div className="item" key={i} onClick={() => this.removeItem(i)}>
+            {item}
+          </div>
+        </CSSTransition>
       );
     });
   }
 
   handleChange = (event) => {
-    this.setState({ inputText: event.target.value })
-  }
+    this.setState({ inputText: event.target.value });
+  };
 
   removeItem(i) {
     let newItems = this.state.items.slice();
     newItems.splice(i, 1);
     this.setState({
-      items: newItems
+      items: newItems,
     });
   }
 
@@ -48,18 +45,21 @@ class App extends Component {
       <div className="App">
         <div className="Animation-App">
           <form onSubmit={this.handleSubmit}>
-            <input placeholder="Enter new Item" type="text" onChange={this.handleChange} value={this.state.inputText} />
-            <input type="submit" value="Add new Item"/>
+            <input
+              placeholder="Enter new Item"
+              type="text"
+              onChange={this.handleChange}
+              value={this.state.inputText}
+            />
+            <input type="submit" value="Add new Item" />
           </form>
-          <ReactCSSTransitionGroup transitionName="AppExp">
+          <TransitionGroup>
             {this.renderItems()}
-          </ReactCSSTransitionGroup>
+          </TransitionGroup>
         </div>
       </div>
     );
   }
 }
-
-
 
 export default App;
